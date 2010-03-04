373a2a987fe903e98a3c75a16544ef835417f4b8
Chris Conrad <cconrad@linkedin.com>
2010-03-04 10:28:33 -0800

    Adding support for Connected message in ZooKeeperManager

--- cluster/src/main/scala/com/linkedin/norbert/cluster/ZooKeeperManagerComponent.scala
+++ cluster/src/main/scala/com/linkedin/norbert/cluster/ZooKeeperManagerComponent.scala
@@ -1,41 +1,118 @@
+/*
+ * Copyright 2009-2010 LinkedIn, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.linkedin.norbert.cluster
 
 import com.linkedin.norbert.util.Logging
 import actors.Actor
-import org.apache.zookeeper.{WatchedEvent, ZooKeeper, Watcher}
+import Actor._
+import java.io.IOException
+import org.apache.zookeeper._
 
 trait ZooKeeperManagerComponent {
   this: ClusterNotificationManagerComponent =>
 
-  sealed trait ZooKeeperMessage
-  object ZooKeeperMessages {
-    case object Connected extends ZooKeeperMessage
-    case object Disconnected extends ZooKeeperMessage
-    case object Expired extends ZooKeeperMessage
-    case class NodeChildrenChanged(path: String) extends ZooKeeperMessage
+  sealed trait ZooKeeperManagerMessage
+  object ZooKeeperManagerMessages {
+    case object Connected extends ZooKeeperManagerMessage
+    case object Disconnected extends ZooKeeperManagerMessage
+    case object Expired extends ZooKeeperManagerMessage
+    case class NodeChildrenChanged(path: String) extends ZooKeeperManagerMessage
+    case object Shutdown extends ZooKeeperManagerMessage
   }
 
-  class ZooKeeperManager(zooKeeperUrls: String, sessionTimeout: Int, clusterName: String)(implicit zooKeeperFactory: (String, Int, Watcher) => ZooKeeper)
-          extends Actor with Logging {
+  class ZooKeeperManager(connectString: String, sessionTimeout: Int, clusterName: String, clusterNotificationManager: Actor)
+          (implicit zooKeeperFactory: (String, Int, Watcher) => ZooKeeper) extends Actor with Logging {
+    private val CLUSTER_NODE = "/" + clusterName
+    private val AVAILABILITY_NODE = CLUSTER_NODE + "/available"
+    private val MEMBERSHIP_NODE = CLUSTER_NODE + "/members"
+
     private var zooKeeper: Option[ZooKeeper] = None
+    private var currentNodes: Seq[Node] = Nil
 
     def act() = {
+      startZooKeeper
+      
       while(true) {
-        import ZooKeeperMessages._
+        import ZooKeeperManagerMessages._
 
         receive {
+          case Connected => handleConnected
           case m => log.error("Received unknown message: %s", m)
         }
       }
     }
+
+    private def handleConnected {
+      zooKeeper match {
+        case Some(zk) =>
+          verifyZooKeeperStructure(zk)
+          lookupCurrentNodes(zk)
+          clusterNotificationManager ! ClusterNotificationMessages.Connected(currentNodes)
+
+        case None =>
+          log.error("Received a Connected message when ZooKeeper is None")
+      }      
+    }
+
+    private def startZooKeeper {
+      zooKeeper = try {
+        Some(zooKeeperFactory(connectString, sessionTimeout, new ClusterWatcher(self)))
+      } catch {
+        case ex: IOException =>
+          log.error(ex, "Unable to connect to ZooKeeper")
+          None
+
+        case ex: Exception =>
+          log.fatal(ex, "Exception while connecting to ZooKeeper")
+          None
+      }
+    }
+
+    private def verifyZooKeeperStructure(zk: ZooKeeper) {
+      log.ifDebug("Verifying ZooKeeper structure...")
+
+      List(CLUSTER_NODE, AVAILABILITY_NODE, MEMBERSHIP_NODE).foreach { path =>
+        log.ifDebug("Ensuring %s exists", path)
+        if (zk.exists(path, false) == null) {
+          log.ifDebug("%s doesn't exist, creating", path)
+          zk.create(path, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+        }
+      }      
+    }
+
+    private def lookupCurrentNodes(zk: ZooKeeper) {
+      import scala.collection.jcl.Conversions._
+
+      val members = zk.getChildren(MEMBERSHIP_NODE, true)
+      val available = zk.getChildren(AVAILABILITY_NODE, true)
+
+      currentNodes = members.map { member =>
+        Node(member.toInt, zk.getData("%s/%s".format(MEMBERSHIP_NODE, member), false, null), available.contains(member))
+      }
+    }
   }
 
+  private implicit def defaultZooKeeperFactory(connectString: String, sessionTimeout: Int, watcher: Watcher) = new ZooKeeper(connectString, sessionTimeout, watcher)
+  
   class ClusterWatcher(zooKeeperManager: Actor) extends Watcher {
     @volatile private var shutdownSwitch = false
 
     def process(event: WatchedEvent) {
       import org.apache.zookeeper.Watcher.Event.{EventType, KeeperState}
-      import ZooKeeperMessages._
+      import ZooKeeperManagerMessages._
 
       if (shutdownSwitch) return
 
--- cluster/src/test/scala/com/linkedin/norbert/cluster/ZooKeeperManagerComponentSpec.scala
+++ cluster/src/test/scala/com/linkedin/norbert/cluster/ZooKeeperManagerComponentSpec.scala
@@ -1,17 +1,168 @@
+/*
+ * Copyright 2009-2010 LinkedIn, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.linkedin.norbert.cluster
 
 import org.specs.SpecificationWithJUnit
 import org.specs.mock.Mockito
 import actors.Actor
-import org.apache.zookeeper.WatchedEvent
+import Actor._
 import org.specs.util.WaitFor
+import org.apache.zookeeper.data.Stat
+import org.apache.zookeeper._
+import java.util.ArrayList
 
 class ZooKeeperManagerComponentSpec extends SpecificationWithJUnit with Mockito with WaitFor with ZooKeeperManagerComponent
         with ClusterNotificationManagerComponent with RouterFactoryComponent {
-  import ZooKeeperMessages._
+  import ZooKeeperManagerMessages._
 
   type Id = Int
   val routerFactory = null
+
+  "ZooKeeperManager" should {
+    val mockZooKeeper = mock[ZooKeeper]
+
+    var connectedCount = 0
+    var nodesReceived: Seq[Node] = Nil
+
+    val notificationActor = actor {
+      react {
+        case ClusterNotificationMessages.Connected(nodes) => connectedCount += 1; nodesReceived = nodes
+      }
+    }
+
+    def zkf(connectString: String, sessionTimeout: Int, watcher: Watcher) = mockZooKeeper
+    val zooKeeperManager = new ZooKeeperManager("", 0, "test", notificationActor)(zkf _)
+    zooKeeperManager.start
+
+    val rootNode = "/test"
+    val membershipNode = rootNode + "/members"
+    val availabilityNode = rootNode + "/available"
+
+    "instantiate a ZooKeeper instance when started" in {
+      var callCount = 0
+      def countedZkf(connectString: String, sessionTimeout: Int, watcher: Watcher) = {
+        callCount += 1
+        mockZooKeeper
+      }
+
+      val zkm = new ZooKeeperManager("", 0, "", notificationActor)(countedZkf _)
+      zkm.start
+      waitFor(10.ms)
+
+      callCount must be_==(1)
+    }
+
+    "when a Connected message is received" in {
+      "verify the ZooKeeper structure by" in {
+        val znodes = List(rootNode, membershipNode, availabilityNode)
+
+        "doing nothing if all znodes exist" in {
+          znodes.foreach(mockZooKeeper.exists(_, false) returns mock[Stat])
+
+          zooKeeperManager ! Connected
+
+          znodes.foreach(mockZooKeeper.exists(_, false) was called)
+        }
+
+        "creating the cluster, membership and availability znodes if they do not already exist" in {
+          znodes.foreach { path =>
+            mockZooKeeper.exists(path, false) returns null
+            mockZooKeeper.create(path, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT) returns path
+          }
+
+          zooKeeperManager ! Connected
+          waitFor(10.ms)
+
+          znodes.foreach { path =>
+            mockZooKeeper.exists(path, false) was called
+            mockZooKeeper.create(path, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT) was called
+          }
+        }
+      }
+
+      "calculate the current nodes" in {
+        val membership = new ArrayList[String]
+        membership.add("1")
+        membership.add("2")
+        membership.add("3")
+
+        val availability = membership.clone.asInstanceOf[ArrayList[String]]
+        availability.remove(2)
+
+        val nodes = Array(Node(1, "localhost", 31313, Array(1, 2), true),
+          Node(2, "localhost", 31314, Array(2, 3), false), Node(3, "localhost", 31315, Array(2, 3), true))
+
+        mockZooKeeper.getChildren(membershipNode, true) returns membership
+        nodes.foreach { node =>
+          mockZooKeeper.getData("%s/%d".format(membershipNode, node.id), false, null) returns Node.nodeToByteArray(node)
+        }
+        mockZooKeeper.getChildren(availabilityNode, true) returns availability
+
+        zooKeeperManager ! Connected
+        waitFor(10.ms)
+
+        mockZooKeeper.getChildren(membershipNode, true) was called
+        nodes.foreach { node =>
+          mockZooKeeper.getData("%s/%d".format(membershipNode, node.id), false, null) was called
+        }
+        mockZooKeeper.getChildren(availabilityNode, true) was called          
+      }
+
+      "send a notification to the notification manager actor" in {
+        val membership = new ArrayList[String]
+        membership.add("1")
+        membership.add("2")
+        membership.add("3")
+
+        val availability = membership.clone.asInstanceOf[ArrayList[String]]
+        availability.remove(2)
+
+        val nodes = Array(Node(1, "localhost", 31313, Array(1, 2), true),
+          Node(2, "localhost", 31314, Array(2, 3), false), Node(3, "localhost", 31315, Array(2, 3), true))
+
+        mockZooKeeper.getChildren(membershipNode, true) returns membership
+        nodes.foreach { node =>
+          mockZooKeeper.getData("%s/%d".format(membershipNode, node.id), false, null) returns Node.nodeToByteArray(node)
+        }
+        mockZooKeeper.getChildren(availabilityNode, true) returns availability
+
+        zooKeeperManager ! Connected
+        waitFor(10.ms)
+
+        connectedCount must be_==(1)
+        nodesReceived must haveTheSameElementsAs(nodes)
+      }
+    }
+
+    "when a Disconnected message is received" in {
+
+    }
+
+    "when an Expired message is received" in {
+
+    }
+
+    "when a NodeChildrenChanged message is received" in {
+
+    }
+
+    "when a Shutdown message is received" in {
+      
+    }
+  }
 
   "ClusterWatcher" should {
     import org.apache.zookeeper.Watcher.Event.{EventType, KeeperState}
@@ -22,8 +173,8 @@ class ZooKeeperManagerComponentSpec extends SpecificationWithJUnit with Mockito 
     var nodesChangedCount = 0
     var nodesChangedPath = ""
 
-    val zkm = Actor.actor {
-      Actor.react {
+    val zkm = actor {
+      react {
         case Connected => connectedCount += 1
         case Disconnected => disconnectedCount += 1
         case Expired => expiredCount += 1
