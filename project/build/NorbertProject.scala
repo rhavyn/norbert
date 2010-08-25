464fa406fcd80b3df8a379900f06c5d63efa20d6
Chris Conrad <cconrad@linkedin.com>
2010-08-25 10:41:23 -0700

    Fix sbt idea plugin and update to scala 2.8.0 final

--- project/build/NorbertProject.scala
+++ project/build/NorbertProject.scala
@@ -1,15 +1,15 @@
 import sbt._
 
-class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlugin {
+class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
   override def repositories = Set(ScalaToolsSnapshots, "JBoss Maven 2 Repository" at "http://repository.jboss.com/maven2")
 
   lazy val cluster = project("cluster", "Norbert Cluster", new ClusterProject(_))
   lazy val network = project("network", "Norbert Network", new NetworkProject(_), cluster)
-  lazy val javaCluster = project("java-cluster", "Norbert Java Cluster", new DefaultProject(_) with IdeaPlugin, cluster)
-  lazy val javaNetwork = project("java-network", "Norbert Java Network", new DefaultProject(_) with IdeaPlugin, javaCluster, network)
-  lazy val examples = project("examples", "Norbert Examples", new DefaultProject(_) with IdeaPlugin, network, javaNetwork)
+  lazy val javaCluster = project("java-cluster", "Norbert Java Cluster", new DefaultProject(_) with IdeaProject, cluster)
+  lazy val javaNetwork = project("java-network", "Norbert Java Network", new DefaultProject(_) with IdeaProject, javaCluster, network)
+  lazy val examples = project("examples", "Norbert Examples", new DefaultProject(_) with IdeaProject, network, javaNetwork)
 
-  class ClusterProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+  class ClusterProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
     val zookeeper = "org.apache.hadoop" % "zookeeper" % "3.3.0" from "http://repo1.maven.org/maven2/org/apache/hadoop/zookeeper/3.3.0/zookeeper-3.3.0.jar"
     val protobuf = "com.google.protobuf" % "protobuf-java" % "2.3.0"
     val log4j = "log4j" % "log4j" % "1.2.14"
@@ -20,7 +20,7 @@ class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlu
     val objenesis = "org.objenesis" % "objenesis" % "1.0" % "test"
   }
 
-  class NetworkProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+  class NetworkProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
     val netty = "org.jboss.netty" % "netty" % "3.1.5.GA"
     val slf4j = "org.slf4j" % "slf4j-api" % "1.5.6"
     val slf4jLog4j = "org.slf4j" % "slf4j-log4j12" % "1.5.6"
